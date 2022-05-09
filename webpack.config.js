9732bce8169edda5b6ff614e6312d2b30dd804c1
Andrey Kulish <andreykulish89@gmail.com>
2022-05-10 00:02:54 +0300

    Feat: webpack-configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/webpack-demo/",
   },
   plugins: [
     new HTMLWebpackPlugin({ template: "./src/index.html" }),
