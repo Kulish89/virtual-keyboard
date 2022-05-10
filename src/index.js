fe2677c30b2708c17d66980d67b793e93b6c146a
Andrey Kulish <andreykulish89@gmail.com>
2022-05-10 16:57:47 +0300

    feat: textarea

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import "./styles/style.css";
 import { BUTTONS } from "./buttons.js";
 const ROOT = document.querySelector(".root");
+const metaKeys = ["navigation", "modifier", "editing", "meta", "whitespace"];
 
 const keyboardWrapper = document.createElement("div");
+const textArea = document.createElement("textarea");
+textArea.classList.add("text-area");
+
+ROOT.append(textArea);
 
 keyboardWrapper.className = "keyboard__wrapper";
 BUTTONS.forEach((btn) => {
   let newBtn = document.createElement("div");
   newBtn.classList.add("key");
-  if (
-    btn.description === "navigation" ||
-    btn.description === "modifier" ||
-    btn.description === "editing" ||
-    btn.description === "meta" ||
-    btn.description === "whitespace"
-  ) {
+  if (metaKeys.includes(btn.description)) {
     newBtn.classList.add("meta-key", btn.code);
   } else if (btn.description === "space") {
     newBtn.classList.add(btn.code);
   }
-  newBtn.setAttribute("id", btn.keyCode);
-  newBtn.innerHTML += btn.unicode ? btn.unicode : btn.key[0];
+  newBtn.setAttribute("id", btn.code);
+  newBtn.innerHTML += btn.unicode ? btn.unicode : btn.key[0].toUpperCase();
   keyboardWrapper.append(newBtn);
 });
 ROOT.append(keyboardWrapper);
 window.addEventListener("keydown", function (e) {
-  document.getElementById(e.keyCode).classList.add("active");
+  // e.preventDefault();
+  document.querySelector(`#${e.code}`).classList.add("active");
 });
 window.addEventListener("keyup", function (e) {
-  document.getElementById(e.keyCode).classList.remove("active");
+  textArea.focus();
+  document.querySelector(`#${e.code}`).classList.remove("active");
 });
