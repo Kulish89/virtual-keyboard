95efd5037e5a231e72fdc8bc070ac3ae95f9704a
Andrey Kulish <andreykulish89@gmail.com>
2022-05-11 12:22:05 +0300

    feat: language toggler and capslock-button

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles/style.css";
 import { BUTTONS } from "./buttons.js";
 const ROOT = document.querySelector(".root");
 const metaKeys = ["navigation", "modifier", "editing", "meta", "whitespace"];
-const language = true;
+let language = true;
+let CAPSLOCK = false;
 
 const keyboardWrapper = document.createElement("div");
 const textArea = document.createElement("textarea");
@@ -12,9 +13,8 @@ keyboardWrapper.className = "keyboard__wrapper";
 createButtons(language);
 
 window.addEventListener("keydown", function (e) {
-  // e.preventDefault();
+  e.preventDefault();
   textArea.focus();
-
   clickOnButton(e.code, e);
 });
 
@@ -26,27 +26,49 @@ function clickOnButton(id, event) {
       button.classList.remove("active");
     }, 100);
     if (id > 30 && id !== 8) {
-      if (!event.shiftKey) {
-        textArea.value += button.lastChild.textContent.toLowerCase();
-      } else {
+      if (event.shiftKey || CAPSLOCK) {
         textArea.value += button.firstChild.textContent;
+      } else {
+        textArea.value += button.lastChild.textContent.toLowerCase();
       }
     }
     if (id === 8) {
       textArea.value = textArea.value.substring(0, textArea.value.length - 1);
     }
+    if (id === 20) {
+      CAPSLOCK = !CAPSLOCK;
+    }
   } else {
     let button = BUTTONS.find((btn) => {
       return btn.code == id;
     });
+
     let btnInDOM = document.getElementById(button.keyCode);
     btnInDOM.classList.add("active");
     setTimeout(() => {
       btnInDOM.classList.remove("active");
     }, 100);
+    if (button.keyCode > 30 && button.keyCode !== 8) {
+      if (event.shiftKey || CAPSLOCK) {
+        textArea.value += btnInDOM.firstChild.textContent;
+      } else {
+        textArea.value += btnInDOM.lastChild.textContent.toLowerCase();
+      }
+    }
+    if (button.keyCode === 8) {
+      textArea.value = textArea.value.substring(0, textArea.value.length - 1);
+    }
+    if (button.keyCode === 20) {
+      CAPSLOCK = !CAPSLOCK;
+    }
+    if (button.keyCode === 16 && event.altKey) {
+      language = !language;
+      createButtons(language);
+    }
   }
 }
 function createButtons(language) {
+  keyboardWrapper.innerHTML = "";
   BUTTONS.forEach((btn) => {
     let newBtn = document.createElement("div");
     newBtn.classList.add("key");
@@ -59,9 +81,13 @@ function createButtons(language) {
       newBtn.innerHTML = `${btn.key[1]}<br>${btn.key[0]}`;
     } else {
       if (language) {
-        newBtn.innerHTML += btn.unicode ? btn.unicode : btn.key[1];
+        newBtn.innerHTML += btn.unicode
+          ? btn.unicode
+          : btn.key[0].toUpperCase();
       } else if (!language) {
-        newBtn.innerHTML += btn.unicode ? btn.unicode : btn.key[3];
+        newBtn.innerHTML += btn.unicode
+          ? btn.unicode
+          : btn.key[2].toUpperCase();
       }
     }
     newBtn.setAttribute("id", btn.keyCode);
@@ -72,5 +98,7 @@ function createButtons(language) {
     });
     keyboardWrapper.append(newBtn);
   });
-  ROOT.append(keyboardWrapper);
+  if (ROOT.lastChild.type == "textarea") {
+    ROOT.append(keyboardWrapper);
+  }
 }
